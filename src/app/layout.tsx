30805fd02a6a8806e06cf2f48a87f9f5173b9d81
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-11-07 14:25:23 -0300

    added authentication to the app with clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 
+import { ClerkProvider } from "@clerk/nextjs";
+
 import "./globals.css";
 
 export const metadata: Metadata = {
-    title: "Ai Notion CLone",
+    title: "Ai Notion Clone",
     description: "A notion clone with ai capability",
 };
 
@@ -13,13 +15,15 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <link
-                rel="icon"
-                href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>😎</text></svg>"
-            />
+        <ClerkProvider>
+            <html lang="en">
+                <link
+                    rel="icon"
+                    href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>😎</text></svg>"
+                />
 
-            <body>{children}</body>
-        </html>
+                <body>{children}</body>
+            </html>
+        </ClerkProvider>
     );
 }
