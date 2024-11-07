8af5285079867af416582018a6ba3c25517e5ed5
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-11-07 13:42:06 -0300

    Base setup for a nextjs project
    source syntax youtube channel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    // typed routes, I'll wait until it's not experimental
+    // experimental: {
+    //     typedRoutes: true,
+    // },
 };
 
 export default nextConfig;
